740f6f2bd5f4fe441c815ee4052319799a81a19f
oscariquelme01 <oscariquelme01@gmail.com>
2022-07-05 17:45:48 +0200

    add catpuccin theme

---
+++ theme/theme.lua
@@ -0,0 +1,29 @@
+local theme = {}
+
+theme.border_width = 1
+
+theme.black = "#1e1e28"
+theme.deep_black = "#1b1923"
+theme.white = "#d7dae0"
+theme.light_white = "#fafafa"
+theme.pink = "#f0afe1"
+theme.red = "#e28c8c"
+theme.orange = "#f9c096"
+theme.yellow = "#eadda0"
+theme.green = "#b3e1a3"
+theme.blue = "#a4b9ef"
+theme.purple = "#c6aae8"
+theme.grey = "#6e6c7c"
+
+theme.bg_normal = theme.black
+theme.bg_focus = theme.deep_black
+theme.bg_urgent = theme.white
+theme.bg_minimize = theme.grey
+theme.bg_systray = theme.black
+
+theme.fg_normal = theme.white
+theme.fg_focus = theme.orange
+theme.fg_urgent = theme.red
+theme.fg_minimize = theme.light_white
+
+return theme
