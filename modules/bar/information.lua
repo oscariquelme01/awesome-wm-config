221f3463f67113e5968a0679551ae1c94104a115
oscariquelme01 <oscariquelme01@gmail.com>
2022-07-25 23:09:35 +0200

    add popup to info widget

--- modules/bar/information.lua
+++ modules/bar/information.lua
@@ -1,18 +1,41 @@
--- local awful = require("awful")
+local awful = require("awful")
 local beautiful = require("beautiful")
 local wibox = require("wibox")
+local gears = require("gears")
 
--- local rounded_shape = require("utilities.utils").rounded_shape()
+local dpi = beautiful.xresources.apply_dpi
 
 return function(s)
-    -- local popup = awful.popup {
-    --     ontop = true,
-    --     screen = s,
-    --     visible = false,
-    --     shape = rounded_shape(2),
-    --     offset = { y = 5 },
-    --     border_width = 1,
-    -- }
+
+    -- popup with all the info
+    local popup = awful.popup {
+        ontop = true,
+        screen = s,
+        visible = false,
+        offset = { y = 40, x = 20 },
+        border_width = 1,
+        width = dpi(40),
+        height = dpi(20),
+        shape = gears.shape.rounded_rect,
+        fg = beautiful.text,
+        bg = beautiful.black,
+
+		widget = {
+			{
+				{
+					layout = wibox.layout.align.vertical,
+					expand = "none",
+					wibox.widget{ markup = 'This <i>is</i> a <b>textbox</b>!!!', align  = 'center', valign = 'center', widget = wibox.widget.textbox },
+					wibox.widget{ markup = 'This <i>is</i> a <b>textbox</b>!!!', align  = 'center', valign = 'center', widget = wibox.widget.textbox },
+					wibox.widget{ markup = 'This <i>is</i> a <b>textbox</b>!!!', align  = 'center', valign = 'center', widget = wibox.widget.textbox },
+				},
+                margins = dpi(16),
+				widget = wibox.container.margin,
+			},
+			bg = beautiful.wibar_bg,
+			widget = wibox.container.background,
+		},
+    }
 
     local information_widget = wibox.widget{
         markup = '<span foreground="' .. beautiful.light_white .. '"></span>',
@@ -22,7 +45,26 @@ return function(s)
         widget = wibox.widget.textbox
     }
 
-    -- function information.toggle()
+    -- toggle function necessary for keybind and button press
+    function information_widget.toggle()
+
+        if popup.visible then
+            popup.visible = not popup.visible
+        else
+            -- popup placement
+            awful.placement.top_right(popup, { margins = { top = beautiful.wibar_height + 10, right = 10 }, parent = awful.screen.focused() })
+
+            popup.visible = true
+
+        end
+    end
+
+    -- toggle widget on click
+    information_widget:connect_signal("button::press",
+        function (_, _, _, button)
+            if button == 1 then information_widget.toggle() end
+        end
+    )
 
     return information_widget
 end
