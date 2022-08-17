02cd4e92239a593b6322ace42676f4943e02d258
oscariquelme01 <oscariquelmejato01@gmail.com>
2022-08-17 13:26:32 +0200

    add non-functional rocket icon at the top of wibar

--- icons/init.lua
+++ icons/init.lua
@@ -6,6 +6,7 @@ local dir = gfs.get_configuration_dir() .. "icons/"
 return {
     ghost = dir .. "ghost.svg",
     scared_ghost = dir .. "scared-ghost.svg",
-    circle = dir .. "circle.svg"
+    circle = dir .. "circle.svg",
+    rocket = dir .. "rocket.png"
 
 }
--- modules/bar/bar.lua
+++ modules/bar/bar.lua
@@ -1,11 +1,12 @@
  local wibox = require("wibox")
  local awful = require("awful")
+ local beautiful = require("beautiful")
+ local dpi = beautiful.xresources.apply_dpi
 
  local taglist = require("modules.bar.taglist")
  local clock = require("modules.bar.clock")
+ local launcher = require("modules.bar.launcher")
  local battery = require("modules.bar.battery")
- local beautiful = require("beautiful")
- local dpi = beautiful.xresources.apply_dpi
 
 
  awful.screen.connect_for_each_screen(
@@ -13,17 +14,21 @@
          s.taglist = taglist(s)
          s.clock = clock()
          s.battery = battery()
+         s.launcher = launcher()
 
          -- wibar
          s.wibar = awful.wibar({position = "left", screen = s, stretch = true})
 
          s.wibar:setup {
              { -- Left widgets
-                layout = wibox.layout.fixed.vertical,
+                s.launcher,
+                top = dpi(8),
+                left = dpi(6),
+                right = dpi(6),
+                layout = wibox.layout.margin,
              },
              { -- Middle widgets
                 s.taglist,
-                -- margins = {left = dpi(4), right = dpi(4)},
                 widget = wibox.container.margin
              },
              { -- Right widgets
